3d2a86d102772665aed817e5e468fa24ee7ff2cb
Travis Lane <travis.lane.osx@gmail.com>
2015-02-15 13:11:09 -0800

    adding async

--- include/launcher.h
+++ include/launcher.h
@@ -9,62 +9,60 @@
 
 enum class LauncherState
 {
-    FIRE,
-    LEFT,
-    RIGHT,
-    STOPPED
+  FIRE,
+  LEFT,
+  RIGHT,
+  STOPPED
 };
 
 class Launcher
 {
 private:
 
-	cv::Point GetClosestFace(cv::Mat frame);
+  cv::Point GetClosestFace(cv::Mat frame);
 
-	uv_loop_t *main_loop;
+  uv_loop_t *main_loop;
 
-	uv_thread_t launcher_thread;
-	uv_thread_t camera_thread;
+  uv_thread_t launcher_thread;
+  uv_thread_t camera_thread;
 
-	uv_mutex_t movement_mutex;
-	uv_timer_t stop_timer;
+  uv_mutex_t movement_mutex;
+  uv_timer_t stop_timer;
+  uv_async_t loop_async;
 
-	bool running = false;
-	bool continue_loop = false;
-	LauncherState state = LauncherState::STOPPED;
+  bool running = false;
+  bool continue_loop = false;
+  LauncherState state = LauncherState::STOPPED;
 
-	std::string haar_body_path;
-	std::string haar_face_path;
+  std::string haar_body_path;
+  std::string haar_face_path;
 
-	ml_launcher_t *launcher;
-	int camera_id;
-	int fps;
+  ml_launcher_t *launcher;
+  int camera_id;
+  int fps;
 
-	int center_count = 0;
-	int max_center_count = 5;
-	int diff = 40;
-
-
-	void static RunCamera(void *arg);
-	void static RunLauncher(void *arg);
-	void static Track(Launcher *launcher,
-	                  cv::Rect frame_rect, cv::Rect face_rect);
+  int center_count = 0;
+  int max_center_count = 5;
+  int diff = 40;
 
 
-	void static LauncherStop(uv_timer_t *timer);
-	int static LauncherReset(Launcher *launcher);
-	int static LauncherFire(Launcher *launcher);
-	int static LauncherLeft(Launcher *launcher, int msec);
-	int static LauncherRight(Launcher *launcher, int msec);
+  void static RunCamera(void *arg);
+  void static RunLauncher(void *arg);
+  void static Track(Launcher *launcher,
+                    cv::Rect frame_rect, cv::Rect face_rect);
+
+
+  void static LauncherStop(uv_timer_t *timer);
+  int static LauncherReset(Launcher *launcher);
+  int static LauncherFire(Launcher *launcher);
+  int static LauncherLeft(Launcher *launcher, int msec);
+  int static LauncherRight(Launcher *launcher, int msec);
 
 public:
-	Launcher(uv_loop_t *main_loop,
-	         ml_launcher_t *launcher, int camera_id, int fps,
-	         std::string haar_body_path, std::string haar_face_path);
-	~Launcher();
-	int Start();
-	int Stop();
+  Launcher(uv_loop_t *main_loop,
+           ml_launcher_t *launcher, int camera_id, int fps,
+           std::string haar_body_path, std::string haar_face_path);
+  ~Launcher();
+  int Start();
+  int Stop();
 };
-
-
-
--- src/launcher.cpp
+++ src/launcher.cpp
@@ -21,318 +21,299 @@ Launcher::Launcher(uv_loop_t *main_loop,
                    ml_launcher_t *launcher, int camera_id, int fps,
                    std::string haar_body_path, std::string haar_face_path)
 {
-	int rv;
-	ml_launcher_reference(launcher);
-	rv = ml_launcher_claim(launcher);
-	if (rv != ML_OK)
-	{
-		syslog(LOG_ERR, "Failed to claim launcher.");
-	}
+  int rv;
+  ml_launcher_reference(launcher);
+  rv = ml_launcher_claim(launcher);
+  if (rv != ML_OK) {
+    syslog(LOG_ERR, "Failed to claim launcher.");
+  }
 
-	this->main_loop = main_loop;
-	this->launcher = launcher;
-	this->camera_id = camera_id;
-	this->fps = fps;
-	this->haar_body_path = haar_body_path;
-	this->haar_face_path = haar_face_path;
+  this->main_loop = main_loop;
+  this->launcher = launcher;
+  this->camera_id = camera_id;
+  this->fps = fps;
+  this->haar_body_path = haar_body_path;
+  this->haar_face_path = haar_face_path;
 
-	uv_mutex_init(&(this->movement_mutex));
+  uv_mutex_init(&(this->movement_mutex));
 
 }
 
 Launcher::~Launcher()
 {
-	if (launcher != NULL)
-	{
-		ml_launcher_unclaim(launcher);
-		ml_launcher_dereference(launcher);
-	}
+  if (launcher != NULL) {
+    ml_launcher_unclaim(launcher);
+    ml_launcher_dereference(launcher);
+  }
 }
 
 int
 Launcher::Start()
 {
-	int rv = -100;
-	uv_mutex_lock(&this->movement_mutex);
-	if (running)
-	{
-		rv = 0;
-		goto out;
-	}
-	continue_loop = true;
+  int rv = -100;
+  uv_mutex_lock(&this->movement_mutex);
+  if (running) {
+    rv = 0;
+    goto out;
+  }
+  continue_loop = true;
 
-	rv = uv_thread_create(&(this->camera_thread),
-	                      Launcher::RunCamera, this);
-	if (rv != 0)
-	{
-		goto out;
-	}
+  uv_async_init(this->main_loop, &(this->loop_async), NULL);
 
-	rv = uv_thread_create(&(this->launcher_thread),
-	                      Launcher::RunLauncher, this);
+  rv = uv_thread_create(&(this->camera_thread),
+                        Launcher::RunCamera, this);
+  if (rv != 0) {
+    goto out;
+  }
 
+  rv = uv_thread_create(&(this->launcher_thread),
+                        Launcher::RunLauncher, this);
 out:
-	if (rv != 0)
-	{
-		continue_loop = false;
-	}
-	uv_mutex_unlock(&this->movement_mutex);
-	return rv;
+  if (rv != 0) {
+    continue_loop = false;
+  }
+  uv_mutex_unlock(&this->movement_mutex);
+  return rv;
 }
 
 int
 Launcher::Stop()
 {
-	int rv = -100;
-	uv_mutex_lock(&(this->movement_mutex));
-	if (!running)
-	{
-		rv = 0;
-		goto out;
-	}
-	continue_loop = false;
-	rv = uv_thread_join(&(this->launcher_thread));
+  int rv = -100;
+  uv_mutex_lock(&(this->movement_mutex));
+  if (!running) {
+    rv = 0;
+    goto out;
+  }
+  continue_loop = false;
+  rv = uv_thread_join(&(this->camera_thread));
+  if (rv != 0) {
+    uv_thread_join(&(this->launcher_thread));
+  } else {
+    rv = uv_thread_join(&(this->launcher_thread));
+  }
 
 out:
-	uv_mutex_unlock(&this->movement_mutex);
-	return rv;
+  uv_mutex_unlock(&this->movement_mutex);
+  return rv;
 }
 
 void
 Launcher::RunLauncher(void *arg)
 {
-	Launcher *launcher = static_cast<Launcher *>(arg);
-	uv_loop_t loop;
-	uv_loop_init(&loop);
+  Launcher *launcher = static_cast<Launcher *>(arg);
+  uv_loop_t loop;
+  uv_loop_init(&loop);
 
-	uv_timer_init(&loop, &(launcher->stop_timer));
-	launcher->stop_timer.data = launcher;
+  uv_timer_init(&loop, &(launcher->stop_timer));
+  launcher->stop_timer.data = launcher;
 
-	while (launcher->continue_loop)
-	{
-		uv_run(&loop, UV_RUN_DEFAULT);
-	}
+  while (launcher->continue_loop) {
+    uv_run(&loop, UV_RUN_DEFAULT);
+  }
 }
 
 void
 Launcher::RunCamera(void *arg)
 {
-	Launcher *launcher = static_cast<Launcher *>(arg);
-	vector<Mat> images;
-	vector<int> labels;
-	CascadeClassifier body_cascade, face_cascade;
+  Launcher *launcher = static_cast<Launcher *>(arg);
+  vector<Mat> images;
+  vector<int> labels;
+  CascadeClassifier body_cascade, face_cascade;
 
-	body_cascade.load(launcher->haar_body_path);
-	face_cascade.load(launcher->haar_face_path);
+  body_cascade.load(launcher->haar_body_path);
+  face_cascade.load(launcher->haar_face_path);
 
-	// Reset the launcher.
-	LauncherReset(launcher);
+  // Reset the launcher.
+  LauncherReset(launcher);
 
-	VideoCapture cap(launcher->camera_id);
-	if (!cap.isOpened())
-	{
-		syslog(LOG_ERR, "Failed to open capture device.");
-	}
+  VideoCapture cap(launcher->camera_id);
+  if (!cap.isOpened()) {
+    syslog(LOG_ERR, "Failed to open capture device.");
+  }
 
-	cap.set(CV_CAP_PROP_FPS, launcher->fps);
+  cap.set(CV_CAP_PROP_FPS, launcher->fps);
 
-	while (launcher->continue_loop)
-	{
-		Rect frame_rect;
-		Mat gray_frame, orig_frame;
+  while (launcher->continue_loop) {
+    Rect frame_rect;
+    Mat gray_frame, orig_frame;
 
-		vector< Rect_<int> > bodies;
+    vector< Rect_<int> > bodies;
 
-		if (!cap.grab())
-			continue;
+    if (!cap.grab()) {
+      continue;
+    }
 
-		if (!cap.retrieve(orig_frame))
-			continue;
+    if (!cap.retrieve(orig_frame)) {
+      continue;
+    }
 
-		frame_rect = Rect(0, 0, orig_frame.cols, orig_frame.rows);
+    frame_rect = Rect(0, 0, orig_frame.cols, orig_frame.rows);
 
-		cvtColor(orig_frame, gray_frame, CV_BGR2GRAY);
-		body_cascade.detectMultiScale(gray_frame, bodies);
+    cvtColor(orig_frame, gray_frame, CV_BGR2GRAY);
+    body_cascade.detectMultiScale(gray_frame, bodies);
 
-		// No bodies foudn.
-		if (bodies.size() == 0)
-			continue;
+    // No bodies foudn.
+    if (bodies.size() == 0) {
+      continue;
+    }
 
-		for (size_t i = 0; i < bodies.size(); i++)
-		{
-			Rect face;
-			vector< Rect_<int> > faces;
-			Mat body_frame = gray_frame(bodies[i]);
+    for (size_t i = 0; i < bodies.size(); i++) {
+      Rect face;
+      vector< Rect_<int> > faces;
+      Mat body_frame = gray_frame(bodies[i]);
 
-			// Make sure there is actually a face in the body.
-			// The body detector seems a bit weak.
-			face_cascade.detectMultiScale(body_frame, faces);
+      // Make sure there is actually a face in the body.
+      // The body detector seems a bit weak.
+      face_cascade.detectMultiScale(body_frame, faces);
 
-			// No faces found.
-			if (faces.size() == 0)
-				continue;
+      // No faces found.
+      if (faces.size() == 0) {
+        continue;
+      }
 
-			// Use the first face found, and set it's offset.
-			face = faces[0];
-			face.x += bodies[i].x;
-			face.y += bodies[i].y;
+      // Use the first face found, and set it's offset.
+      face = faces[0];
+      face.x += bodies[i].x;
+      face.y += bodies[i].y;
 
-			Track(launcher, frame_rect, face);
-		}
-	}
+      Track(launcher, frame_rect, face);
+    }
+  }
 }
 
 void
 Launcher::Track(Launcher *launcher, Rect frame_rect, Rect face_rect)
 {
-	int center_x, face_center_x, face_diff_x;
+  int center_x, face_center_x, face_diff_x;
 
-	center_x = frame_rect.width / 2;
-	face_center_x = face_rect.x + (face_rect.width / 2);
-	face_diff_x = center_x - face_center_x;
+  center_x = frame_rect.width / 2;
+  face_center_x = face_rect.x + (face_rect.width / 2);
+  face_diff_x = center_x - face_center_x;
 
-	if (abs(face_diff_x) <= launcher->diff)
-	{
-		// Centered
-		if (launcher->center_count++ == launcher->max_center_count)
-		{
-			launcher->center_count = 0;
-			LauncherFire(launcher);
-		}
-	}
-	else
-	{
-		int msec = 0;
+  if (abs(face_diff_x) <= launcher->diff) {
+    // Centered
+    if (launcher->center_count++ == launcher->max_center_count) {
+      launcher->center_count = 0;
+      LauncherFire(launcher);
+    }
+  } else {
+    int msec = 0;
 
-		if (face_diff_x < center_x / 8)
-		{
-			msec = 40;
-		}
-		else if (face_diff_x < center_x / 4)
-		{
-			msec = 80;
-		}
-		else
-		{
-			msec = 120;
-		}
+    if (face_diff_x < center_x / 8) {
+      msec = 40;
+    } else if (face_diff_x < center_x / 4) {
+      msec = 80;
+    } else {
+      msec = 120;
+    }
 
-		// Not Centered.
-		launcher->center_count = 0;
-		if (face_center_x > center_x)
-		{
-			LauncherRight(launcher, msec);
-		}
-		else
-		{
-			LauncherLeft(launcher, msec);
-		}
-	}
+    // Not Centered.
+    launcher->center_count = 0;
+    if (face_center_x > center_x) {
+      LauncherRight(launcher, msec);
+    } else {
+      LauncherLeft(launcher, msec);
+    }
+  }
 }
 
 void
 Launcher::LauncherStop(uv_timer_t *timer)
 {
-	int rv;
-	Launcher *launcher = static_cast<Launcher *>(timer->data);
+  int rv;
+  Launcher *launcher = static_cast<Launcher *>(timer->data);
 
-	uv_mutex_lock(&(launcher->movement_mutex));
+  uv_mutex_lock(&(launcher->movement_mutex));
 
-	rv = ml_launcher_stop(launcher->launcher);
-	if (rv != ML_OK)
-	{
-		syslog(LOG_NOTICE, "Failed to stop launcher.");
-	}
+  rv = ml_launcher_stop(launcher->launcher);
+  if (rv != ML_OK) {
+    syslog(LOG_NOTICE, "Failed to stop launcher.");
+  }
 
-	launcher->state = LauncherState::STOPPED;
+  launcher->state = LauncherState::STOPPED;
 
-	uv_mutex_unlock(&(launcher->movement_mutex));
+  uv_mutex_unlock(&(launcher->movement_mutex));
 }
 
 int
 Launcher::LauncherFire(Launcher *launcher)
 {
-	int rv;
+  int rv;
 
-	uv_mutex_lock(&(launcher->movement_mutex));
+  uv_mutex_lock(&(launcher->movement_mutex));
 
-	if (launcher->state != LauncherState::STOPPED)
-	{
-		rv = -1;
-		goto out;
-	}
+  if (launcher->state != LauncherState::STOPPED) {
+    rv = -1;
+    goto out;
+  }
 
-	rv = ml_launcher_fire(launcher->launcher);
-	if (rv != ML_OK)
-	{
-		syslog(LOG_NOTICE, "Failed to fire launcher.");
-		goto out;
-	}
+  rv = ml_launcher_fire(launcher->launcher);
+  if (rv != ML_OK) {
+    syslog(LOG_NOTICE, "Failed to fire launcher.");
+    goto out;
+  }
 
-	launcher->state = LauncherState::FIRE;
-	uv_timer_start(&(launcher->stop_timer),
-	               Launcher::LauncherStop, 5000, 0);
+  launcher->state = LauncherState::FIRE;
+  uv_timer_start(&(launcher->stop_timer),
+                 Launcher::LauncherStop, 5000, 0);
 
 out:
-	uv_mutex_unlock(&(launcher->movement_mutex));
-	return rv;
+  uv_mutex_unlock(&(launcher->movement_mutex));
+  return rv;
 }
 
 int
 Launcher::LauncherRight(Launcher *launcher, int msec)
 {
-	int rv;
+  int rv;
 
-	uv_mutex_lock(&(launcher->movement_mutex));
+  uv_mutex_lock(&(launcher->movement_mutex));
 
-	if (launcher->state != LauncherState::STOPPED)
-	{
-		rv = -1;
-		goto out;
-	}
+  if (launcher->state != LauncherState::STOPPED) {
+    rv = -1;
+    goto out;
+  }
 
-	rv = ml_launcher_move(launcher->launcher, ML_RIGHT);
-	if (rv != ML_OK)
-	{
-		syslog(LOG_NOTICE, "Failed to track right.");
-		goto out;
-	}
+  rv = ml_launcher_move(launcher->launcher, ML_RIGHT);
+  if (rv != ML_OK) {
+    syslog(LOG_NOTICE, "Failed to track right.");
+    goto out;
+  }
 
-	launcher->state = LauncherState::RIGHT;
-	uv_timer_start(&(launcher->stop_timer),
-	               Launcher::LauncherStop, msec, 0);
+  launcher->state = LauncherState::RIGHT;
+  uv_timer_start(&(launcher->stop_timer),
+                 Launcher::LauncherStop, msec, 0);
 
 out:
-	uv_mutex_unlock(&(launcher->movement_mutex));
-	return rv;
+  uv_mutex_unlock(&(launcher->movement_mutex));
+  return rv;
 }
 
 int
 Launcher::LauncherLeft(Launcher *launcher, int msec)
 {
-	int rv;
+  int rv;
 
-	uv_mutex_lock(&(launcher->movement_mutex));
+  uv_mutex_lock(&(launcher->movement_mutex));
 
-	if (launcher->state != LauncherState::STOPPED)
-	{
-		rv = -1;
-		goto out;
-	}
+  if (launcher->state != LauncherState::STOPPED) {
+    rv = -1;
+    goto out;
+  }
 
-	rv = ml_launcher_move(launcher->launcher, ML_LEFT);
-	if (rv != ML_OK)
-	{
-		syslog(LOG_NOTICE, "Failed to track left.");
-		goto out;
-	}
+  rv = ml_launcher_move(launcher->launcher, ML_LEFT);
+  if (rv != ML_OK) {
+    syslog(LOG_NOTICE, "Failed to track left.");
+    goto out;
+  }
 
-	launcher->state = LauncherState::LEFT;
-	uv_timer_start(&(launcher->stop_timer),
-	               Launcher::LauncherStop, msec, 0);
+  launcher->state = LauncherState::LEFT;
+  uv_timer_start(&(launcher->stop_timer),
+                 Launcher::LauncherStop, msec, 0);
 out:
-	uv_mutex_unlock(&(launcher->movement_mutex));
-	return rv;
+  uv_mutex_unlock(&(launcher->movement_mutex));
+  return rv;
 }
 
 /**
@@ -341,26 +322,24 @@ out:
 int
 Launcher::LauncherReset(Launcher *launcher)
 {
-	int rv;
+  int rv;
 
-	uv_mutex_lock(&(launcher->movement_mutex));
+  uv_mutex_lock(&(launcher->movement_mutex));
 
-	launcher->center_count = 0;
+  launcher->center_count = 0;
 
-	rv = ml_launcher_zero(launcher->launcher);
-	if (rv != ML_OK)
-	{
-		syslog(LOG_NOTICE, "Failed to zero launcher.");
-		goto out;
-	}
+  rv = ml_launcher_zero(launcher->launcher);
+  if (rv != ML_OK) {
+    syslog(LOG_NOTICE, "Failed to zero launcher.");
+    goto out;
+  }
 
-	rv = ml_launcher_move_mseconds(launcher->launcher, ML_UP, 400);
-	if (rv != ML_OK)
-	{
-		syslog(LOG_NOTICE, "Failed to reset launcher angle.");
-	}
+  rv = ml_launcher_move_mseconds(launcher->launcher, ML_UP, 400);
+  if (rv != ML_OK) {
+    syslog(LOG_NOTICE, "Failed to reset launcher angle.");
+  }
 
 out:
-	uv_mutex_unlock(&(launcher->movement_mutex));
-	return rv;
+  uv_mutex_unlock(&(launcher->movement_mutex));
+  return rv;
 }
